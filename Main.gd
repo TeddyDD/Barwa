f7f9d8797d8484ba0436bd9a8ca27c0499f9e601
Daniel Lewan <vision360.daniel@gmail.com>
2018-12-25 01:32:11 +0100

    composing

--- Main.gd
+++ Main.gd
@@ -2,11 +2,38 @@ extends Control
 
 onready var ColorItem = preload("res://controls/Color.tscn")
 
+var palette = {
+	file = null,
+	colors = []
+}
 
-# Called when the node enters the scene tree for the first time.
+var picker = false setget set_picker
+var precise_picker = false setget set_precise_picker
+
 func _ready():
-	pass
+	add_color()
+	
+func add_color():
+	var c = {}
+	c.color = Color(1,1,1,1)
+	c.control = spawn_color_control()
+	
+	
+func spawn_color_control():
+	var c = ColorItem.instance()
+	$"UI/Color List/ScrollContainer/List".add_child(c)
+	return c
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func set_picker(value : bool) -> void:
+	$"UI/Mouse Picker".visible = value
+	picker = value
+	
+func set_precise_picker(value : bool) -> void:
+	$"UI/Precise Picker".visible = value
+	precise_picker = value
+
+# Create new color in palette
+
+func _on_New_pressed():
+	add_color()
