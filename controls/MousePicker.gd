70c36ce7fb5cbbe43abddc34c363068a5a727187
Daniel Lewan <vision360.daniel@gmail.com>
2018-12-27 21:30:26 +0100

    add control off for mouse picker

--- controls/MousePicker.gd
+++ controls/MousePicker.gd
@@ -2,7 +2,8 @@ tool
 extends Control
 
 var id
-var old_color
+var old_color = null setget set_old_color
+
 
 export(NodePath) var wrapper
 
@@ -11,8 +12,11 @@ signal set_color(id, color)
 var color = Color()
 var mouse = false
 export var control = true
+var mouse_pos
 export var saturation_speed = 0.005
 export var safe_margin = 25 # pixels
+
+export(Texture) var color_checker
 
 func _ready():
 	color.s = 1
@@ -20,47 +24,75 @@ func _ready():
 func _gui_input(event):
 	if not mouse:
 		return
-	
 	var updated = false
+	var c = {
+		h=color.h,
+		s=color.s,
+		v=color.v,
+		a=color.a
+	}
 	
-	if event is InputEventMouseButton:
-		var change = 0
-		if control:
+	match event.get_class():
+		"InputEventMouseButton":
 			match event.button_index:
-				BUTTON_WHEEL_UP: change = saturation_speed
-				BUTTON_WHEEL_DOWN: change = -saturation_speed
-				BUTTON_RIGHT:
-					mouse = false
+				BUTTON_LEFT:
+					if event.is_pressed():
+						control = !control
+						mouse_pos = event.position
 					accept_event()
+				BUTTON_WHEEL_UP: 
+					c.s = clamp(color.s + saturation_speed, 0, 1)
+					updated = true
+				BUTTON_WHEEL_DOWN: 
+					c.s = clamp(color.s - saturation_speed, 0, 1) 
+					updated = true
+				BUTTON_RIGHT:
+					accept_event()
+					mouse = false
+					control = false
 					_color_accepted()
-					return
-			var s = clamp(color.s + change, 0, 1)
-			color = Color.from_hsv(color.h, s, color.v, 1.0)
-			updated = true
-	
-	if event is InputEventMouse and control:
-		var r = get_rect()
-		var h = range_lerp(event.position.x, safe_margin, r.size.x - safe_margin, 0.0, 1.0)
-		var v = range_lerp(event.position.y, safe_margin, r.size.y - safe_margin, 0.0, 1.0)
-		color = Color.from_hsv(h, color.s, v, 1.0)
-		updated = true
-		
-	if updated:
+		"InputEventMouseMotion":
+			if control == true:
+				mouse_pos = event.position
+				_pos_to_color(event.position, c)
+				updated = true
+	if updated == true:
+		color = Color.from_hsv(c.h, c.s, c.v, c.a)
 		accept_event()
 		update()
 		$Label.text = str(color.to_html(false))
+		
+func _color_to_pos(color):
+	var v = Vector2()
+	v.x = range_lerp(color.h, 0.0, 1.0, safe_margin, get_rect().size.x - safe_margin)
+	v.y = range_lerp(color.v, 0.0, 1.0, safe_margin, get_rect().size.y - safe_margin)
+	return v
+	
+func _pos_to_color(pos, color):
+	var r = get_rect()
+	color.h = range_lerp(pos.x, safe_margin, r.size.x - safe_margin, 0.0, 1.0)
+	color.v = range_lerp(pos.y, safe_margin, r.size.y - safe_margin, 0.0, 1.0)
+	
+func set_old_color(c):
+	old_color = c
+	if c:
+		mouse_pos = _color_to_pos(c)
+		color = c
+		control = false
 
 func _color_accepted():
+	assert(id != null)
+	assert(color != null)
 	emit_signal("set_color", id, color)
 	id = null
 	old_color = null
 	get_node(wrapper).visible = false
 
 func _draw():
+	draw_texture_rect(color_checker, get_rect(), true)
 	draw_rect(get_rect(), color)
-	var p = get_local_mouse_position()
+	var p = mouse_pos if mouse_pos else get_local_mouse_position()
 	draw_circle(p, 3, color.inverted())
-	draw_line(p - Vector2(-30,0), p + Vector2(30,0), color.inverted(), 5, true)
 
 func _on_MousePicker_mouse_entered():
 	mouse = true
